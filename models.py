f8040a8736ff22517ec8760c434d0789337b54d0
PawloNazaruk <pawlonazaruk@gmail.com>
2023-08-29 18:24:51 +0200

    Model Task created

---
+++ models.py
@@ -0,0 +1,31 @@
+
+# Models of tasks used by Todo App
+
+from database import Base
+from sqlalchemy import TIMESTAMP, Column, String, Boolean, Integer, BigInteger
+from sqlalchemy.sql import func
+
+
+class Note(Base):
+    __tablename__ = 'notes'
+    id = Column(BigInteger().with_variant(Integer, "sqlite"), primary_key=True)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    category = Column(String, nullable=True)
+    published = Column(Boolean, nullable=False, default=True)
+    createdAt = Column(TIMESTAMP(timezone=True), nullable=False, server_default=func.now())
+    updatedAt = Column(TIMESTAMP(timezone=True), default=None, onupdate=func.now())
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+    id = Column(BigInteger().with_variant(Integer, "sqlite"), primary_key=True)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=True)
+    priority = Column()
+    category = Column(String, nullable=True)
+    completed = Column(Boolean, nullable=False)
+    createdAt = Column(TIMESTAMP(timezone=True), nullable=False, server_default=func.now())
+    updatedAt = Column(TIMESTAMP(timezone=True), default=None, onupdate=func.now())
+    finishUntil = Column(TIMESTAMP(timezone=True), nullable=False)
+    completedAt = Column(TIMESTAMP(timezone=True), default=None)
