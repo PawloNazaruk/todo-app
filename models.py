f53a7deb8a9a2b8799cf3ae555c4dc7349d97d78
PawloNazaruk <pawlonazaruk@gmail.com>
2023-08-29 18:40:17 +0200

    Added Enum class for priority Column of Task Model

--- models.py
+++ models.py
@@ -1,20 +1,16 @@
 
 # Models of tasks used by Todo App
+from enum import Enum
 
 from database import Base
 from sqlalchemy import TIMESTAMP, Column, String, Boolean, Integer, BigInteger
 from sqlalchemy.sql import func
 
 
-class Note(Base):
-    __tablename__ = 'notes'
-    id = Column(BigInteger().with_variant(Integer, "sqlite"), primary_key=True)
-    title = Column(String, nullable=False)
-    content = Column(String, nullable=False)
-    category = Column(String, nullable=True)
-    published = Column(Boolean, nullable=False, default=True)
-    createdAt = Column(TIMESTAMP(timezone=True), nullable=False, server_default=func.now())
-    updatedAt = Column(TIMESTAMP(timezone=True), default=None, onupdate=func.now())
+class Priority(Enum):
+    HIGH: str = "high"
+    MEDIUM: str = "medium"
+    LOW: str = "low"
 
 
 class Task(Base):
@@ -22,7 +18,7 @@ class Task(Base):
     id = Column(BigInteger().with_variant(Integer, "sqlite"), primary_key=True)
     title = Column(String, nullable=False)
     content = Column(String, nullable=True)
-    priority = Column()
+    priority = Column(Priority, nullable=False)
     category = Column(String, nullable=True)
     completed = Column(Boolean, nullable=False)
     createdAt = Column(TIMESTAMP(timezone=True), nullable=False, server_default=func.now())
