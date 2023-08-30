12a7437ba9f840c454ab53881e8dae43de0bc46c
PawloNazaruk <pawlonazaruk@gmail.com>
2023-08-30 18:58:56 +0200

    TaskSchema created

--- schemas.py
+++ schemas.py
@@ -4,3 +4,23 @@
 from datetime import datetime
 from typing import List
 from pydantic import BaseModel
+
+from models import Priority
+
+
+class TaskSchema(BaseModel):
+    id: int = None
+    title: str
+    content: str = None
+    priority: Priority
+    category: str = None
+    completed: str = None  # czy lepiej 0?
+    createdAt: datetime
+    updatedAt: datetime
+    finishUntil: datetime
+    completedAt: datetime
+
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
